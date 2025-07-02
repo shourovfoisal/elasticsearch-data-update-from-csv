b71ba50031be484bcee4e7b3fba9178a6aeba891
shourov.foisal <shourov.foisal@technonext.com>
2025-07-02 18:08:41 +0600

    display percentage progress

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pandas as pd
+import math
+from time import time
 from configs import es_index, es_client
 from process_data import process_data
 
 # Read file
-dataframe = pd.read_csv("input/data.csv", nrows=200, encoding="utf8")
+dataframe = pd.read_csv("input/data.csv", encoding="utf8")
 
 # Updatable columns
 columnNames = dataframe.columns.tolist()
@@ -12,6 +14,9 @@ if "id" in columnNames: columnNames.remove("id")
 totalCount = len(dataframe)
 tenPercentOfTotalCount = int(totalCount/10)
 
+start_time = time()
+
+last_percentage = -1
 for index, row in dataframe.iterrows():
   doc = dict()
   for columnName in columnNames:
@@ -20,4 +25,9 @@ for index, row in dataframe.iterrows():
   payload = {"doc": doc}
   es_client.update(index=es_index, id=row['id'].replace(",", ""), body=payload)
   
-  if((index + 1) % tenPercentOfTotalCount == 0): print(f"Progress {int(((index + 1) * 100) / totalCount)}%", )
+  current_percentage = math.ceil(((index+1) / totalCount) * 100)
+  if(current_percentage % 5 == 0 and current_percentage != last_percentage):
+    print(f"Progress {current_percentage}%")
+    last_percentage = current_percentage
+
+print(f"\nElapsed time {round(time() - start_time, 2)} seconds")
