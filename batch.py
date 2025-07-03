0479445282ecc2c0178e1929d715cc116b15584a
shourov.foisal <shourov.foisal@technonext.com>
2025-07-03 16:13:43 +0600

    add new batch update feature

--- batch.py
+++ batch.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import math
 from time import time
-from configs import es_index, es_client
+from configs import es_index, es_client, batch_size
 from process_data import process_data
 
 # Read file
@@ -17,23 +17,33 @@ tenPercentOfTotalCount = int(totalCount/10)
 start_time = time()
 last_percentage = -1
 
+request_body_ndjson = []
 for index, row in dataframe.iterrows():
   doc = dict()
   for columnName in columnNames:
     doc[columnName]=process_data(columnName, row[columnName])
   
+  document_id = row['id'].replace(",", "")
+  metadata = { "update": { "_id": document_id } }
   payload = {"doc": doc}
+  request_body_ndjson.append(metadata)
+  request_body_ndjson.append(payload)
   
-  try:
-    es_client.update(index=es_index, id=row['id'].replace(",", ""), body=payload)
-  except:
-    print(f"Error while updating document with id {row['id']}")
-    print("Payload:")
-    print(payload)
+  if((index+1) % batch_size == 0):
+    try:
+      es_client.bulk(index=es_index, body=request_body_ndjson)
+      request_body_ndjson.clear()
+    except:
+      print(f"Error while updating document with id {row['id']}")
+      print("Payload:")
+      print(payload)
   
   current_percentage = math.ceil(((index+1) / totalCount) * 100)
   if(current_percentage % 5 == 0 and current_percentage != last_percentage):
     print(f"Progress {current_percentage}%")
     last_percentage = current_percentage
+
+# Upload the remaining
+es_client.bulk(index=es_index, body=request_body_ndjson)
 
 print(f"\nElapsed time {round(time() - start_time, 2)} seconds")
