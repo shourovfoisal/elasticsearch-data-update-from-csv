e820916e90c4c83f99e8a5252cfac928a31cb82b
shourov.foisal <shourov.foisal@technonext.com>
2025-08-21 17:01:35 +0600

    Comprehensive variable name

--- helpers/payload_constructor.py
+++ helpers/payload_constructor.py
@@ -1,7 +1,13 @@
+import os
+
+add_upon_failure = bool(os.getenv("ES_ADD_DOCUMENT_IF_NOT_EXISTS"))
 
 def prepare_payload(row, doc, request_body_ndjson):
   document_id = row['id'].replace(",", "")
   metadata = { "update": { "_id": document_id } }
   payload = {"doc": doc}
+  
+  if(add_upon_failure): payload['doc_as_upsert'] = True
+  
   request_body_ndjson.append(metadata)
   request_body_ndjson.append(payload)
